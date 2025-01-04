a959993430344f5454eb14908fc78560e2db593c
Mancy <mostafamance24@gmail.com>
2025-01-04 22:54:17 +0200

    adding home page locators

--- src/main/java/Pages/P02_HomePage.java
+++ src/main/java/Pages/P02_HomePage.java
@@ -1,17 +1,58 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class P02_HomePage {
 
     //Locators
+    private final By users = By.xpath("//app-aside/ul/li[2]/a");
+    private final By controls = By.xpath("//app-aside/ul/li[3]/div/a");
+    private final By roles =By.xpath("//app-aside/ul/li[3]/div//div/a[1]");
+    private final By permission =By.xpath("//app-aside/ul/li[3]/div//div/a[2]");
+    private final By members =By.xpath("//app-aside/ul/li[4]/a");
+    private final By inheritors =By.xpath("//app-aside/ul/li[5]/a");
+    private final By sarfyia =By.xpath("//app-aside/ul/li[6]/div/a");
+    private final By paymentPayroll =By.xpath("//app-aside/ul/li[6]/div/div/a[1]");
+    private final By paymentPrepare =By.xpath("//app-aside/ul/li[6]/div/div/a[2]");
+    private final By paymentSarfiaat =By.xpath("//app-aside/ul/li[6]/div/div/a[3]");
+    private final By paymentPrepareReport =By.xpath("//app-aside/ul/li[6]/div/div/a[4]");
+    private final By visas =By.xpath("//app-aside/ul/li[7]/div/a");
+    private final By visasExports =By.xpath("//app-aside/ul/li[7]/div/div/a[1]");
+    private final By visasImportBankFiles =By.xpath("//app-aside/ul/li[7]/div/div/a[2]");
+    private final By visaFolders =By.xpath("//app-aside/ul/li[7]/div/div/a[3]");
+    private final By visaFoldersDeliveries =By.xpath("//app-aside/ul/li[7]/div/div/a[4]");
+    private final By visaBankTransactions =By.xpath("//app-aside/ul/li[7]/div/div/a[5]");
+    private final By visaActivations =By.xpath("//app-aside/ul/li[7]/div/div/a[6]");
+    private final By reports =By.xpath("//app-aside/ul/li[8]/div/a");
+    private final By reportsVisaDelivery =By.xpath("//app-aside/ul/li[8]/div/div/a[1]");
+    private final By reportsPayments =By.xpath("//app-aside/ul/li[8]/div/div/a[2]");
+    private final By reportsSubventions=By.xpath("//app-aside/ul/li[8]/div/div/a[3]");
+    private final By reportsIncentives=By.xpath("//app-aside/ul/li[8]/div/div/a[4]");
+    private final By reportsWalletTransactions=By.xpath("//app-aside/ul/li[8]/div/div/a[5]");
+    private final By refund=By.xpath("//app-aside/ul/li[9]/a");
+    private final By depositsRefund=By.xpath("//app-aside/ul/li[10]/a");
+    private final By subventions=By.xpath("//app-aside/ul/li[11]/a");
+    private final By Inquiries=By.xpath("//app-aside/ul/li[12]/div/a");
+    private final By InquiriesBankData=By.xpath("//app-aside/ul/li[12]/div/div/a");
+    private final By Delegates=By.xpath("//app-aside/ul/li[13]/a");
+    private final By tickets=By.xpath("//app-aside/ul/li[14]/a");
+    private final By subscriptions=By.xpath("//app-aside/ul/li[15]/div/a");
+    private final By subscriptionsReceipts=By.xpath("//app-aside/ul/li[15]/div/div/a[1]");
+    private final By subscriptionsSubscriptions=By.xpath("//app-aside/ul/li[15]/div/div/a[2]");
+    private final By subscriptionsSCards=By.xpath("//app-aside/ul/li[15]/div/div/a[3]");
+    private final By subscriptionsSRePrent=By.xpath("//app-aside/ul/li[15]/div/div/a[4]");
+    private final By subscriptionsSMemberImages=By.xpath("//app-aside/ul/li[15]/div/div/a[5]");
+    private final By subscriptionsArchives=By.xpath("//app-aside/ul/li[15]/div/div/a[6]");
 
     //variables
     private final WebDriver driver;
+
     //constructors
     public P02_HomePage(WebDriver driver) {
         this.driver=driver;
     }
+
     //Action
 
     //Assertions
